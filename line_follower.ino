f19791a97e5cb5e6be5a7e97f4ac92c3076eb149
Rusu Cosmin-Constantin <57033890+rusucc@users.noreply.github.com>
2023-05-16 20:44:53 +0300

    Update line_follower.ino
    Adaugat senzori laterali

--- line_follower.ino
+++ line_follower.ino
@@ -4,30 +4,78 @@ Rusu Cosmin, Dogaru Paul, Zem 2023 LineFollower
 Pini Senzori : D4,D5,D6,D7,D8,D9, Emiter Pin
 Pini Led : D2, D15
 Pini Motor(A si B) : D10,D11
-Pini Senzori laterali : A6,A7 I2C
-Pini Liberi : A0->A5,  
+Pini Senzori laterali : A6,A7 I2C, A0,A1 XSHUT
+Pini Liberi : A2->A5,  
 */
 #include <QTRSensors.h>
+#include <Adafruit_VL53L0X.h>
+//constante PID
 #define SPEED 100
 #define KP 0
 #define KI 0
 #define KD 0
-
+//constante senzori laterali
+#define L1_ADDRESS 0x30
+#define L2_ADDRESS 0x31
+#define SHT_LOX1 A0
+#define SHT_LOX2 A1
+//constante senzori pololu
 #define SensorCount 6
-#define LedA null
-#define LedB null
-
-#define MotorA null
-#define MotorB null
-
 #define SensorStart 4
 #define SensorEmitter 3
-
+//leduri
+#define LedA null
+#define LedB null
+//motoare
+#define In1A null
+#define In2A null
+#define In1B null
+#define In2B null
+#define motorA 10
+#define motorB 11
+//debug
 const bool DEBUG_MODE = false;
+//senzori pololu
 QTRSensors qtr;
-
 uint16_t sensorValues[SensorCount];
+Adafruit_VL53L0X lox1 = Adafruit_VL53L0X();
+Adafruit_VL53L0X lox2 = Adafruit_VL53L0X();
+//senzori laterali
+VL53L0X_RangingMeasurementData_t measure1;
+VL53L0X_RangingMeasurementData_t measure2;
 
+void setup();
+void setID();
+void read_dual_sensors();
+int PID();
+void loop()
+{
+  uint16_t currentPosition = qtr.readLineBlack(sensorValues);
+  int valPID = PID(2500,currentPosition); //valoare PID output
+  valPID=map(valPID, -2500, 2500, 0,255);
+  analogWrite(motorA,SPEED-valPID);
+  analogWrite(motorB,SPEED+valPID);
+  delay(100);
+  if(DEBUG_MODE){
+    for (uint8_t i = 0; i < SensorCount; i++){
+      Serial.print(sensorValues[i]);
+      Serial.print('\t');
+    }
+    Serial.print("| |Pozitie: ");
+    Serial.print(currentPosition),Serial.print("| Valoare PID: "),Serial.println(valPID);
+  }
+}
+int PID(int targetPosition, int currentPosition){
+  static int INTEGRAL = 0;
+  static int delta_vechi = 0;
+  int delta = currentPosition - targetPosition;
+  INTEGRAL += delta;
+  int P = KP * delta;
+  int I = KI * INTEGRAL;
+  int D = KD * (delta-delta_vechi);
+  delta_vechi = delta;
+  return P+I+D;
+}
 void setup()
 {
   // configure the sensors
@@ -59,34 +107,69 @@ void setup()
     Serial.println();
     delay(1000);
   }
-}
-int PID(int targetPosition, int currentPosition){
-  static int INTEGRAL = 0;
-  static int delta_vechi = 0;
-  int delta = currentPosition - targetPosition;
-  INTEGRAL += delta;
-  int P = KP * delta;
-  int I = KI * INTEGRAL;
+  pinMode(SHT_LOX1, OUTPUT);
+  pinMode(SHT_LOX2, OUTPUT);
+  if(DEBUG_MODE) Serial.println(F("Shutdown pins inited..."));
+  digitalWrite(SHT_LOX1, LOW);
+  digitalWrite(SHT_LOX2, LOW);
+  if(DEBUG_MODE) Serial.println(F("Both in reset mode...(pins are low)"));
+  if(DEBUG_MODE) Serial.println(F("Starting..."));
+  setID();
 
-  
-  int D = KD * (delta-delta_vechi);
-  delta_vechi = delta;
-  return P+I+D;
 }
-void loop()
-{
-  uint16_t currentPosition = qtr.readLineBlack(sensorValues);
-  int vPID = PID(2500,currentPosition); //valoare PID output
-  if(DEBUG_MODE){
-    for (uint8_t i = 0; i < SensorCount; i++){
-      Serial.print(sensorValues[i]);
-      Serial.print('\t');
-    }
-    Serial.print("| |Pozitie: ")
-    Serial.print(currentPosition),Serial.print("| Valoare PID: "),Serial.println(vPid);
+void setID() {
+  // all reset
+  digitalWrite(SHT_LOX1, LOW);    
+  digitalWrite(SHT_LOX2, LOW);
+  delay(10);
+  // all unreset
+  digitalWrite(SHT_LOX1, HIGH);
+  digitalWrite(SHT_LOX2, HIGH);
+  delay(10);
+
+  // activating LOX1 and resetting LOX2
+  digitalWrite(SHT_LOX1, HIGH);
+  digitalWrite(SHT_LOX2, LOW);
+
+  // initing LOX1
+  if(!lox1.begin(L1_ADDRESS)) {
+    Serial.println(F("Failed to boot first VL53L0X"));
+    while(1);
   }
-  vPid=map(vPID, -2500, 2500, 0,255);
-  analogWrite(motorA,speed-vPID);
-  analogWrite(motorB,speed+vPID);
-  delay(100);
+  delay(10);
+
+  // activating LOX2
+  digitalWrite(SHT_LOX2, HIGH);
+  delay(10);
+
+  //initing LOX2
+  if(!lox2.begin(L2_ADDRESS)) {
+    Serial.println(F("Failed to boot second VL53L0X"));
+    while(1);
+  }
+}
+void read_dual_sensors() {
+  
+  lox1.rangingTest(&measure1, false); // pass in 'true' to get debug data printout!
+  lox2.rangingTest(&measure2, false); // pass in 'true' to get debug data printout!
+
+  // print sensor one reading
+  if(DEBUG_MODE){
+    Serial.print(F("1: "));
+    if(measure1.RangeStatus != 4) {     // if not out of range
+      Serial.print(measure1.RangeMilliMeter);
+    } else {
+      Serial.print(F("Out of range"));
+    }
+  
+    Serial.print(F(" "));
+    // print sensor two reading
+    Serial.print(F("2: "));
+    if(measure2.RangeStatus != 4) {
+      Serial.print(measure2.RangeMilliMeter);
+    } else {
+      Serial.print(F("Out of range"));
+    }
+    Serial.println();
+  }
 }
