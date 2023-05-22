f9e539b09770121be70068a53540eb0bbcca8c8e
Rusu Cosmin-Constantin <57033890+rusucc@users.noreply.github.com>
2023-05-23 00:23:22 +0300

    Update line_follower.ino

--- line_follower.ino
+++ line_follower.ino
@@ -13,10 +13,13 @@ Pini Liberi : A2->A5,
 //debug
 const bool DEBUG_MODE = false ;
 
-#define SPEED 180 //190
-#define KP 0.010 //0.01 
-#define KI 0.00025 //0.003
-#define KD 0.0025 //0.03
+#define SPEED 178 //190
+#define KP 0.01 //0.01 
+#define KI 0.00 //0.003
+#define KD 0.01 //0.03
+#define NOISE 40
+#define BOOST 30
+#define DELTASPEED 8
 //constante senzori laterali
 #define L1_ADDRESS 0x30
 #define L2_ADDRESS 0x31
@@ -25,8 +28,9 @@ const bool DEBUG_MODE = false ;
 //constante senzori pololu
 #define SensorCount 6
 #define SensorStart 4
-#define SensorEmitter 13 //initial D3!!!!
+#define SensorEmitter 3 //initial D3!!!!
 #define TCALIBRARE 100
+#define THRESHOLD 900
 //leduri
 #define LedA 2
 #define LedB 12
@@ -37,6 +41,7 @@ const bool DEBUG_MODE = false ;
 //senzori pololu
 QTRSensors qtr;
 uint16_t sensorValues[SensorCount];
+bool lineValues[SensorCount];
 Adafruit_VL53L0X lox1 = Adafruit_VL53L0X();
 Adafruit_VL53L0X lox2 = Adafruit_VL53L0X();
 //senzori laterali
@@ -46,11 +51,13 @@ VL53L0X_RangingMeasurementData_t measure2;
 void setup();
 void setID();
 void read_dual_sensors();
+int lineCase();
 int PID(int targetPosition, int currentPosition){
   //Serial.println("POZ:"+String(currentPosition));
   static int INTEGRAL = 0;
   static int delta_vechi = 0;
   double delta = currentPosition - targetPosition;
+  if(abs(delta)<NOISE) delta = 0;
   INTEGRAL += delta;
   double P = KP * delta;
   double I = KI * INTEGRAL;
@@ -65,11 +72,16 @@ int PID(int targetPosition, int currentPosition){
 void loop()
 {
   uint16_t currentPosition = qtr.readLineBlack(sensorValues);
+  for(int i = 0; i < SensorCount; i++) lineValues[i] = (sensorValues[i]>THRESHOLD)? true:false;
   int valPID;
+  int lCase = lineCase();
   valPID = PID(2500,currentPosition); //valoare PID output
   //Serial.println(valPID);
-  int valA = SPEED + valPID;
-  int valB = SPEED - valPID;
+  int adder = 0;
+  if(abs(valPID)<DELTASPEED) adder = BOOST;
+  else adder = 0;
+  int valA = SPEED + valPID + adder;
+  int valB = SPEED - valPID + adder;
   if(valA<128) valA=128;
   if(valB<128) valB=128;
   if(valA>255) valA = 255;
@@ -180,6 +192,36 @@ void setID() {
   if(!lox2.begin(L2_ADDRESS)) {
     Serial.println(F("Failed to boot second VL53L0X"));
     while(1);
+  }
+}
+int lineCase(){
+  /*
+   * Comentarii ca sa nu ma dau cu capul de pereti
+   * 1. Linie dreapta
+   * 2. Cruce
+   * 3. Senzor lateral stanga 
+   * 4. Senzor lateral dreapta
+   * 5. Senzor perete stanga
+   * 6. Senzor perete dreapta
+   * 
+   * 3 sau 4: iesire din giratoriu/linie punctata -> mers spre linie
+   * 5 sau 6:
+   */
+  bool pol = false;
+  for(int i=0; i<SensorCount; i++) pol = (pol || lineValues[i]);
+  bool wallL = false, wallR = false;
+  bool lineR = false, lineL = false;
+  if(pol){ // giratoriu, cruce, linie normala
+    if(lineR or lineL){
+      if(lineR and lineL) return 2; // cruce
+      else if(lineL) return 3; //posibil line punctata
+      else return 4; //giratoriu sau lineR true;
+    }
+    else return 1; //linie normala
+  }
+  else{
+    if(wallL) return 5;
+    if(wallR) return 6;
   }
 }
 void read_dual_sensors() {
